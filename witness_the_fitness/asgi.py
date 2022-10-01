77009b32c20cbdf548401e8d655b06e633f51e4c
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-01 11:20:51 +0000

    Initial commit

---
+++ witness_the_fitness/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for witness_the_fitness project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'witness_the_fitness.settings')
+
+application = get_asgi_application()
