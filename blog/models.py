ff1359fc8c567a86163656c41484e778366ed8ec
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-01 20:43:37 +0000

    Add blog app post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from trainers.models import Trainers
+from profiles.models import UserProfile
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(Trainers, on_delete=models.CASCADE, related_name="blog_posts")
+    created_on = models.DateField(auto_now_add=True)
+    featured_image = models.ImageField(default="blog_placeholder")
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(UserProfile, related_name="post_likes", blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
