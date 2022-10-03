9c1feda5dbb1639ccd0c927de62bf1ee58a8b779
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-03 19:53:57 +0000

    Add post detail template, view and url

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post, Comment
 
 
 class PostList(generic.ListView):
@@ -8,3 +8,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/posts.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    Recipe detail view to display full recipe when clicked on.
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
