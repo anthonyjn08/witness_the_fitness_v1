2fa0e3868b5a34ad4b36583486ff84df9a8e9b4d
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-04 19:33:20 +0000

    Add edit and delete post urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('add_post/', views.add_post, name='add_post'),
+    path('edit_post/<slug:slug>', views.EditPost.as_view(),
+                    name='edit_post'),
+    path('delete_post/<slug:slug>', views.DeletePost.as_view(),
+                    name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
