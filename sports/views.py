9398264987505cff700d6571f58082d0c701f48e
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-06 19:15:37 +0000

    Add all_sports view

--- sports/views.py
+++ sports/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Sports
 
-# Create your views here.
+
+def all_sports(request):
+
+    sports = Sports.objects.all()
+
+    context = {
+        'sports': sports
+    }
+
+    return render(request, 'sports/sports.html/', context)
