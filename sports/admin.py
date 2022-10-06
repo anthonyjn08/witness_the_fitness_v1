f33ba8d45fc61dc9703ef55b819c62fab7791a95
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-06 19:11:35 +0000

    Add sport app admin

--- sports/admin.py
+++ sports/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Classes
 
-# Register your models here.
+
+@admin.register(Sports)
+class SportsAdmin(admin.ModelAdmin):
+
+    list_display = ('sport_category', 'sport', 'trainer_id', 'sport_cost')
+    search_fields = ['sport_category', 'sport']
--- sports/models.py
+++ sports/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from trainers.models import Trainers
 
-# Create your models here.
+class Sports(models.Model):
+    trainer_id = models.ForeignKey(Trainers, on_delete=models.CASCADE)
+    sport = models.CharField(max_length=100, null=False, blank=False)
+    sport_category = models.CharField(max_length=100, null=False, blank=False)
+    sport_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+    sport_location = models.CharField(max_length=100, null=False, blank=False)
+    sport_image = models.ImageField(default='placeholder')
+
+    def __str__(self):
+        return self.sport
+    
+    class Meta:
+        verbose_name_plural = 'Sports'
