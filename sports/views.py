5282d970018181ade301f9ed62675b2f12dd3ccd
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-08 10:45:31 +0000

    Add search functionality

--- sports/views.py
+++ sports/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Sports
 
 
 def all_sports(request):
     """ A view to show all sports """
     sports = Sports.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('classes'))
+            
+            queries = Q(sport_category__icontains=query) | Q(sport_description__icontains=query)
+            sports = sports.filter(queries)
 
     context = {
-        'sports': sports
+        'sports': sports,
+        'search_term': query,
     }
 
     return render(request, 'sports/sports.html/', context)
