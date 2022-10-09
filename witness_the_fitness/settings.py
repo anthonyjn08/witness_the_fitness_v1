c979e8a84926230e588c4a3a2b28363bbf2bbef2
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-09 10:10:01 +0000

    Add allowed hosts in settings.py

--- witness_the_fitness/settings.py
+++ witness_the_fitness/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0=cj@4z+6(lm8w8d3c$3-g@ds#god@!$uv*=pab)&82g1e!4t-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['witness-the-fitness-ajn.herokuapp.com', 'localhost']
 
 
 # Application definition
