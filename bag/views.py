8ca63be740568941df282e72e12c5969109c5dbb
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-10-31 20:32:37 +0000

    Add bag views and template

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_bag(request):
+    """ A view that returns the shopping bag """
+
+    return render(request, 'bag/bag.html')
--- witness_the_fitness/urls.py
+++ witness_the_fitness/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('trainers/', include('trainers.urls')),
     path('classes/', include('sports.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
