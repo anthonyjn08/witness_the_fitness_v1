0d18dd2e409467271febdb4025deef54758c3679
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-11-07 20:01:03 +0000

    Added messages css, shopping bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from sports.models import Sports
@@ -13,13 +13,15 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add specified class to the shopping bag """
 
-    sport = Sports.objects.get(pk=item_id)
+    sport = get_object_or_404(Sports, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {sport.sport_category } quantity to {bag[item_id]}')
+
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {sport.sport_category } to your bag')
@@ -31,13 +33,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of specified sport in the shopping bag """
 
+    sport = get_object_or_404(Sports, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {sport.sport_category } quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {sport.sport_category } from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -46,10 +51,16 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Remove an item from the shopping bag """
 
-    bag = request.session.get('bag', {})
+    try:
+        sport = get_object_or_404(Sports, pk=item_id)
+        bag = request.session.get('bag', {})
 
-    bag.pop(item_id)
+        bag.pop(item_id)
+        messages.success(request, f'Removed {sport.sport_category } from your bag')
 
-    request.session['bag'] = bag
-    return HttpResponse(status=200)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
 
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
