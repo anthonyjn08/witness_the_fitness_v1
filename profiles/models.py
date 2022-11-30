368ce232778e2818ed08a085f2d1453be74dca91
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-11-30 22:04:31 +0000

    Update profiles model and create profile view

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
+    """
+    A user profile model for maintaining order history
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -16,3 +21,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
--- witness_the_fitness/urls.py
+++ witness_the_fitness/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('classes/', include('sports.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
