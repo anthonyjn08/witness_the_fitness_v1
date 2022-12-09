a09dfbb94cb8be7b6a4b351e0b0714ddd8082591
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-09 21:21:54 +0000

    Change image field using clearablefileinput widget

--- sports/forms.py
+++ sports/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Sports
 
 
@@ -7,6 +8,8 @@ class SportForm(forms.ModelForm):
     class Meta:
         model = Sports
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
