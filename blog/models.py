355695375a46e9dc356c4543324597991a609976
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-10 18:53:36 +0000

    Update name to foreign key in blog comment model

--- blog/models.py
+++ blog/models.py
@@ -38,8 +38,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=80)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="user_comments")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
