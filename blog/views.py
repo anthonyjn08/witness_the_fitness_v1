7b652f2b7ea6735a99081aa223d135184cf8b71b
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-10 21:18:57 +0000

    Update post detail view in blog views.py to include comments

--- blog/views.py
+++ blog/views.py
@@ -69,13 +69,42 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        return render(
-            request,
-            "blog/post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked,
-                "comment_form": CommentForm(),
-            },
-        )
+        template = 'blog/post_detail.html'
+        context = {
+            "post": post,
+            "comments": comments,
+            "liked": liked,
+            "comment_form": CommentForm(),
+        }
+
+        return render(request, template, context)
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.name = request.user
+            comment.save()
+            messages.success(request, 'Thanks for your comment')
+
+        else:
+            comment_form = CommentForm()
+
+        template = 'blog/post_detail.html'
+        context = {
+            "post": post,
+            "comments": comments,
+            "liked": liked,
+            "comment_form": CommentForm(),
+        }
+
+        return render(request, template, context)
