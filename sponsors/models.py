4f40e86f128b8a9ac1c630ad0de14192353505e4
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-13 21:18:05 +0000

    Add sponsors model

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Sponsors(models.Model):
+    sponsor_name = models.CharField(max_length=100, null=False, blank=False)
+    sponsor_bio = models.TextField(null=False)
+    sponsor_image = models.ImageField(null=True, blank=True)
+    sponsor_website = models.TextField(null=False)
+
+    def __str__(self):
+        return self.sponsor_name
+
+    class Meta:
+        db_table = 'Sponsors'
+        # Add verbose name
+        verbose_name = 'Sponsor'
