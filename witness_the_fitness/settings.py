d4b37166ac5c487723faad45c10df8da251ec5cb
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-13 00:30:01 +0000

    Removed secret key

--- witness_the_fitness/settings.py
+++ witness_the_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0=cj@4z+6(lm8w8d3c$3-g@ds#god@!$uv*=pab)&82g1e!4t-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['witness-the-fitness-ajn.herokuapp.com', 'localhost']
 
