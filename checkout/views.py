93c8261374effa27f471fe3c5efe9710bbd2fef1
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-14 19:38:48 +0000

    Flake8 fixes for checkout and bag apps

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+    )
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -12,6 +14,7 @@ from bag.contexts import bag_contents
 
 import stripe
 import json
+
 
 @require_POST
 def cache_checkout_data(request):
@@ -69,21 +72,24 @@ def checkout(request):
                         order_line_item.save()
                 except Sports.DoesNotExist:
                     messages.error(request, (
-                        "One of the sports in your bag wasn't found in our database. "
+                        "One of the sports in your bag wasn't found in our \
+                            database."
                         "Please contact us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success',
+                            args=[order.order_number]))
         else:
             messages.error(request, "There was an error with your form. \
             Please double check your information.")
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
+            messages.error(request, "There's nothing in your bag at the \
+                moment")
             return redirect(reverse('sports'))
 
         current_bag = bag_contents(request)
@@ -95,7 +101,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form with any info the user maintains in
+        # their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
@@ -161,7 +168,7 @@ def checkout_success(request, order_number):
 
     if 'bag' in request.session:
         del request.session['bag']
-    
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,6 +7,7 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
+
 @require_POST
 @csrf_exempt
 def webhook(request):
@@ -39,7 +40,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.
+        handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
