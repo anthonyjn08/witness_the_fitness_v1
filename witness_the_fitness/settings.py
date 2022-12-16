89b3f6491b7daf1410d6047b02e367347a1fac6f
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-12-16 02:06:54 +0000

    Add bugs and fixes to testing.md

--- witness_the_fitness/settings.py
+++ witness_the_fitness/settings.py
@@ -218,7 +218,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'witnessthefitness@example.com'
+    DEFAULT_FROM_EMAIL = 'witnessthefitnessinc@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
